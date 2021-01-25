aa396b57268ed9d1619a443cce043b69b17d683c
Paolo del Mundo <paolo.delmundo@fool.com>
2021-01-25 09:38:21 -0500

    Adding VAULT_ADDR env Removing values from previous run

--- scripts/step3_application.sh
+++ scripts/step3_application.sh
@@ -1,4 +1,6 @@
-vault unwrap "s.5Vgnk9UnoPgnAG3M7HuKv6gU"
-vault write auth/approle/login role_id="fca5a97b-69ee-8c9a-4814-9c8547056e9a" secret_id="20d15017-a980-f753-ce99-c82de60118b9"
+export VAULT_ADDR='http://127.0.0.1:8200'
+
+vault unwrap ""
+vault write auth/approle/login role_id="" secret_id=""
 # vault token lookup s.wZnkQ4ekLHlg4dD1NAmJqRXk
-VAULT_TOKEN="s.wZnkQ4ekLHlg4dD1NAmJqRXk" vault kv get secret/paolo
+VAULT_TOKEN="" vault kv get secret/paolo
